e888efd88afe39bc71ab7d2389c2516835febe9a
Vatsanand09 <anand@ashmarip.com>
2018-01-12 16:22:25 +0530

    fersh

--- admin/login/config.php
+++
@@ -1,8 +0,0 @@
-<?
-$host = "localhost"; // Hostname
-$port = "3306"; // MySQL Port : Default : 3306
-$user = "username"; // Username Here
-$pass = "password"; //Password Here
-$db   = "database_name"; // Database Name
-$dbh  = new PDO('mysql:dbname='.$db.';host='.$host.';port='.$port,$user,$pass);
-?>
